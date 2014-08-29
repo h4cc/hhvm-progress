7e32a3e1ed8b4a4b8372f9280bfd1ff655cebb24
Julius Beckmann <github@h4cc.de>
2014-08-29 22:25:17 +0200

    Providing SVG badges Delivering SVG batches now by default as described in #38. Old PNG batches still available. Also added a JSON Route for shields.io service.

--- src/h4cc/HHVMProgressBundle/Controller/BadgesController.php
+++ src/h4cc/HHVMProgressBundle/Controller/BadgesController.php
@@ -5,23 +5,44 @@ namespace h4cc\HHVMProgressBundle\Controller;
 use Composer\Package\Version\VersionParser;
 use h4cc\HHVMProgressBundle\Entity\PackageVersion;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class BadgesController extends Controller
 {
-    public function showAction($name, Request $request)
+    public function jsonAction($name, Request $request)
     {
         $branch = $request->get('branch', 'dev-master');
 
-        /** @var \h4cc\HHVMProgressBundle\Entity\PackageVersionRepository $repo */
-        $repo = $this->get('h4cc_hhvm_progress.repos.package_version');
+        $packageVersion = $this->getPackageVersion($name, $branch);
 
-        // Move this instance to a service.
-        $versionParser = new VersionParser();
+        $hhvmStatusString = 'unknown';
+        if($packageVersion) {
+            switch($packageVersion->getHhvmStatus()) {
+                case PackageVersion::HHVM_STATUS_SUPPORTED:
+                    $hhvmStatusString = 'tested';
+                    break;
+                case PackageVersion::HHVM_STATUS_NONE:
+                    $hhvmStatusString = 'not_tested';
+                    break;
+                case PackageVersion::HHVM_STATUS_ALLOWED_FAILURE:
+                    $hhvmStatusString = 'partial';
+                    break;
+            }
+        }
 
-        // Move this logic also to a service?
-        $packageVersion = $repo->get($name, $versionParser->normalize($branch));
+        $response = new JsonResponse(array('hhvm_status' => $hhvmStatusString));
+        $response->headers->set('Cache-Control', sprintf('public, maxage=%s, s-maxage=%s', 3600, 3600));
+
+        return $response;
+    }
+
+    public function showAction($name, Request $request, $type)
+    {
+        $branch = $request->get('branch', 'dev-master');
+
+        $packageVersion = $this->getPackageVersion($name, $branch);
 
         $badgeFile = 'unknown';
         if($packageVersion) {
@@ -38,11 +59,28 @@ class BadgesController extends Controller
             }
         }
 
-        $response = new Response(file_get_contents(__DIR__.'/../Resources/badges/hhvm_'.$badgeFile.'.png'));
+        if('svg' == $type) {
+            $response = new Response(file_get_contents(__DIR__.'/../Resources/badges/hhvm_'.$badgeFile.'.svg'));
+            $response->headers->set('Content-Type', 'image/svg+xml;charset=utf-8');
+        }else{
+            $response = new Response(file_get_contents(__DIR__.'/../Resources/badges/hhvm_'.$badgeFile.'.png'));
+            $response->headers->set('Content-Type', 'image/png');
+        }
 
-        $response->headers->set('Content-Type', 'image/png');
         $response->headers->set('Cache-Control', sprintf('public, maxage=%s, s-maxage=%s', 3600, 3600));
 
         return $response;
+    }
+
+    private function getPackageVersion($name, $branch)
+    {
+        /** @var \h4cc\HHVMProgressBundle\Entity\PackageVersionRepository $repo */
+        $repo = $this->get('h4cc_hhvm_progress.repos.package_version');
+
+        // Move this instance to a service.
+        $versionParser = new VersionParser();
+
+        // Move this logic also to a service?
+        return $repo->get($name, $versionParser->normalize($branch));
     }
 }
