c5b472619330b5432ebfbcf71057f0be55e5d12c
Julius Beckmann <github@h4cc.de>
2014-12-28 21:38:16 +0100

    Updating all packages now.

--- src/h4cc/HHVMProgressBundle/Command/PackageUpdateCommand.php
+++ src/h4cc/HHVMProgressBundle/Command/PackageUpdateCommand.php
@@ -22,7 +22,7 @@ class PackageUpdateCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $local = true;
+        $local = false;
 
         if($local) {
             // Packages for testing all three possible hhvm status.
